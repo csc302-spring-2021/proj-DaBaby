16bdbeebb9bc084cfebddf0455972484cfbdbd22
Jenny <jennny.zhang@mail.utoronto.ca>
2021-02-21 09:14:43 -0500

    Added basic django view test

--- backend/setup/tests.py
+++ backend/setup/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.test import Client
 
 # Create your tests here.
+class IndexViewTests(TestCase):
+    def test_basic(self):
+        response = self.client.get('/api/test/')
+        self.assertEqual(response.status_code, 200)
