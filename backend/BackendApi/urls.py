4f0ed5305110ebaafa5b482e4c62c87d48a802cd
Victor <victor.alvarez@mail.utoronto.ca>
2021-02-20 22:57:00 -0500

    Created the url routes for the sdcform app, created generic views for those urls, and created serializers file

--- backend/BackendApi/urls.py
+++ backend/BackendApi/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("admin/", admin.site.urls),
-    path("api/", views.open_api, name="open-api")
+    path("api/", views.open_api, name="open-api"),
+    path("api/sdcform/", include("sdcform.urls"))
 ]
