787f87fa3ad3873f7aed39cc6c75434cceb032dc
Jenny <jennny.zhang@mail.utoronto.ca>
2021-02-22 10:28:31 -0500

    removed tests

--- sdctools-service/sdcform/tests.py
+++ sdctools-service/sdcform/tests.py
@@ -1,15 +1,3 @@
 from django.test import TestCase
 
 # Create your tests here.
-class SDCFormViewTests(TestCase):
-    
-    def test_valid_form(self):
-        response = self.client.get("/api/sdcform/covid19")
-        self.assertEqual(response.status_code, 200)
-
-    def test_invalid_form(self):
-        response = self.client.get("api/sdcform/foo")
-        self.assertEqual(response.status_code, 404)
-
-    # def test_upload_xml(self):
-    #     response = self.client.post("api/sdcform/", {})
