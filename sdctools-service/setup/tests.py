a7c552e7fcb4e9a2ad55811178fb74dce2223663
Jenny <jennny.zhang@mail.utoronto.ca>
2021-02-22 21:52:07 -0500

    mock endpoint for getting SDCForm with hardcoded data, and unit tests

--- sdctools-service/setup/tests.py
+++ sdctools-service/setup/tests.py
@@ -1,8 +1,27 @@
 from django.test import TestCase
 from django.test import Client
 
+import json
+
 # Create your tests here.
 class IndexViewTests(TestCase):
     def test_basic(self):
         response = self.client.get('/api/test/')
         self.assertEqual(response.status_code, 200)
+
+    def test_valid_form(self):
+
+        response = self.client.get("/api/test/sdcform/covid19/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('sdcFormObject' in json.loads(response.content))
+        self.assertContains(response, 'sdcFormObject')
+
+    def test_invalid_form(self):
+
+        response = self.client.get("api/test/sdcform/foo/")
+        self.assertEqual(response.status_code, 404)
+
+    
+    # def test_upload_xml(self):
+    #     response = self.client.post("api/test/sdcform/", data={}, content_type="application/json")
+    #     self.assertEqual(response.status_code, 201)
