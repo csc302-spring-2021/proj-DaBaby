b4580117ae5f1727a458872d18f1cec10ef71de9
Victor <victor.alvarez@mail.utoronto.ca>
2021-02-23 05:11:20 -0500

    Completed the upload XML API endpoint

--- sdctools-service/sdcform/urls.py
+++ sdctools-service/sdcform/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.sdcforms, name="api-sdcforms"),
     path("<int:sdc_id>/", views.sdcform, name="api-sdcform"),
-    path("<int:procedure_id>/", views.sdcform_by_proc_id, name="api-sdcform-by-proc-id")
+    path("<str:procedure_id>/", views.sdcform_by_proc_id, name="api-sdcform-by-proc-id")
 ]
