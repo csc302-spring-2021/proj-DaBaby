39cfce2e5c175d54dd7d9018d665cf830557954d
Altair59 <39001328+Altair59@users.noreply.github.com>
2021-03-21 18:51:46 -0400

    added label for search bar

--- form-filler/src/components/SDCResponseDashboard.jsx
+++ form-filler/src/components/SDCResponseDashboard.jsx
@@ -6,6 +6,7 @@ import Table from "react-bootstrap/Table";
 import Dropdown from "react-bootstrap/Dropdown";
 import NavItem from "react-bootstrap/NavItem";
 import NavLink from "react-bootstrap/NavLink";
+import "./SDCResponseDashboard.scss";
 
 const hardcodedResponses = [
 	{
@@ -41,14 +42,16 @@ class ResponseDashboard extends React.Component {
 	}
 
 	onSearch() {
-		this.setState({displayedResponses: this.state.responses.filter(
-			resp => resp.diagnosticProcedureID.toUpperCase().indexOf(this.state.searchProcedure) > -1 &&
-				resp.patientID.toUpperCase().indexOf(this.state.searchPatient) > -1 &&
-				((this.state.searchStartDate === "" && this.state.searchEndDate === "") ||
-					(this.state.searchStartDate === "" && new Date(this.state.searchEndDate).toISOString() >= resp.timestamp) ||
-					(this.state.searchEndDate === "" && new Date(this.state.searchStartDate).toISOString() <= resp.timestamp) ||
-					(new Date(this.state.searchEndDate).toISOString() >= resp.timestamp && new Date(this.state.searchStartDate).toISOString() <= resp.timestamp))
-			)})
+		this.setState({
+			displayedResponses: this.state.responses.filter(
+				resp => resp.diagnosticProcedureID.toUpperCase().indexOf(this.state.searchProcedure) > -1 &&
+					resp.patientID.toUpperCase().indexOf(this.state.searchPatient) > -1 &&
+					((this.state.searchStartDate === "" && this.state.searchEndDate === "") ||
+						(this.state.searchStartDate === "" && new Date(this.state.searchEndDate).toISOString() >= resp.timestamp) ||
+						(this.state.searchEndDate === "" && new Date(this.state.searchStartDate).toISOString() <= resp.timestamp) ||
+						(new Date(this.state.searchEndDate).toISOString() >= resp.timestamp && new Date(this.state.searchStartDate).toISOString() <= resp.timestamp))
+			)
+		})
 	}
 
 	onInputProcedure(e) {
@@ -68,70 +71,74 @@ class ResponseDashboard extends React.Component {
 	}
 
 	onDeleteResp(resp) {
-		console.log(`make request to delete resp ${resp.id}`)
+		console.log(`make request to delete response ${resp.id}`)
 	}
+
 	onUpdateResp(resp) {
-		console.log(`make request to update resp ${resp.id}`)
+		console.log(`transfer to fill response ${resp.id}`)
 	}
 
 	render() {
 		return (
-			<div>
-				<div>
-					<h1>Search Previous Responses</h1>
-					<div>
-						<Form>
-							<Form.Row>
-								<Col xs="auto">
-									<Form.Control placeholder="Diagnostic Procedure ID"
-									              onChange={this.onInputProcedure.bind(this)}/>
-								</Col>
-								<Col xs="auto">
-									<Form.Control placeholder="Patient ID" onChange={this.onInputPatient.bind(this)}/>
-								</Col>
-								<Col xs="auto">
-									<Form.Control type={"date"} onChange={this.onInputStartDate.bind(this)}/>
-								</Col>
-								<Col xs="auto">
-									<Form.Control type={"date"} onChange={this.onInputEndDate.bind(this)}/>
-								</Col>
-							</Form.Row>
-							<Button variant="primary"
-							        onClick={this.onSearch.bind(this)}>Search</Button>
-						</Form>
-					</div>
-					<div>
-						<Table striped bordered={false} hover>
-							<thead>
-							<tr>
-								<th>Procedure ID</th>
-								<th>Patient ID</th>
-								<th>Last Edited</th>
-								<th/>
+			<div id="filler-panel">
+				<h2>Search Previous Responses</h2>
+				<div id="filler-panel-head">
+					<Form>
+						<Form.Row>
+							<Col xs="auto">
+								<label>Procedure</label>
+								<Form.Control placeholder="Diagnostic Procedure ID"
+								              onChange={this.onInputProcedure.bind(this)}/>
+							</Col>
+							<Col xs="auto">
+								<label>Patient</label>
+								<Form.Control placeholder="Patient ID" onChange={this.onInputPatient.bind(this)}/>
+							</Col>
+							<Col xs="auto">
+								<label>Start Date</label>
+								<Form.Control type={"date"} onChange={this.onInputStartDate.bind(this)}/>
+							</Col>
+							<Col xs="auto">
+								<label>End Date</label>
+								<Form.Control type={"date"} onChange={this.onInputEndDate.bind(this)}/>
+							</Col>
+						</Form.Row>
+						<Button variant="primary"
+						        onClick={this.onSearch.bind(this)}>Search</Button>
+					</Form>
+				</div>
+				<div id="filler-panel-body">
+					<p>{this.state.displayedResponses.length} Result(s)</p>
+					<Table striped bordered={false} hover>
+						<thead>
+						<tr>
+							<th>Procedure ID</th>
+							<th>Patient ID</th>
+							<th>Last Edited</th>
+							<th/>
+						</tr>
+						</thead>
+						<tbody>
+						{this.state.displayedResponses.map(resp => (
+							<tr key={resp.id}>
+								<td>{resp.diagnosticProcedureID}</td>
+								<td>{resp.patientID}</td>
+								<td>{resp.timestamp}</td>
+								<td>
+									<Dropdown as={NavItem}>
+										<Dropdown.Toggle as={NavLink}>Edit</Dropdown.Toggle>
+										<Dropdown.Menu>
+											<Dropdown.Item
+												onClick={this.onDeleteResp.bind(this, resp)}>Delete</Dropdown.Item>
+											<Dropdown.Item
+												onClick={this.onUpdateResp.bind(this, resp)}>Update</Dropdown.Item>
+										</Dropdown.Menu>
+									</Dropdown>
+								</td>
 							</tr>
-							</thead>
-							<tbody>
-							{this.state.displayedResponses.map(resp => (
-								<tr>
-									<td>{resp.diagnosticProcedureID}</td>
-									<td>{resp.patientID}</td>
-									<td>{resp.timestamp}</td>
-									<td>
-										<Dropdown as={NavItem}>
-											<Dropdown.Toggle as={NavLink}>Edit</Dropdown.Toggle>
-											<Dropdown.Menu>
-												<Dropdown.Item
-													onClick={this.onDeleteResp.bind(this, resp)}>Delete</Dropdown.Item>
-												<Dropdown.Item
-													onClick={this.onUpdateResp.bind(this, resp)}>Update</Dropdown.Item>
-											</Dropdown.Menu>
-										</Dropdown>
-									</td>
-								</tr>
-							))}
-							</tbody>
-						</Table>
-					</div>
+						))}
+						</tbody>
+					</Table>
 				</div>
 			</div>
 		);
