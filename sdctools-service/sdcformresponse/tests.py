e6c1c03824377d8ed682bc359196538f5cf73788
Jenny <jennny.zhang@mail.utoronto.ca>
2021-03-21 10:16:55 -0400

    commented out tests for functions that have not been implemented

--- sdctools-service/sdcformresponse/tests.py
+++ sdctools-service/sdcformresponse/tests.py
@@ -56,6 +56,7 @@ class CreateNewResponseTests(TestCase):
 
     def test_invalid_patientID(self):
         self.client = Client()
+        formID = upload_initial_form() 
         data = { 
             "patientID": "OH2789",
             "clinicianID": "CAMD92378223",
@@ -67,10 +68,11 @@ class CreateNewResponseTests(TestCase):
 
     def test_invalid_clinicianID(self):
         self.client = Client()
+        formID = upload_initial_form() 
         data = { 
             "patientID": "OH27891892",
             "clinicianID": "CAMD",
-            "sdcFormID": 1234,
+            "sdcFormID": formID,
         }
 
         response = self.client.post("/api/sdcformresponse/", data)
@@ -88,104 +90,105 @@ class CreateNewResponseTests(TestCase):
         self.assertEqual(response.status_code, 201) 
         self.assertTrue('responseObject' in json.loads(response.content))
 
+# ===== Uncomment below as functions are added ======= #
 
-class GetResponseTests(TestCase):
+# class GetResponseTests(TestCase):
 
-    def test_get_all_responses(self):
-        self.client = Client()
-        response = self.client.get('/api/sdcformresponse/')
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue('sdcFormResponses' in json.loads(response.content))
+#     def test_get_all_responses(self):
+#         self.client = Client()
+#         response = self.client.get('/api/sdcformresponse/')
+#         self.assertEqual(response.status_code, 200)
+#         self.assertTrue('sdcFormResponses' in json.loads(response.content))
 
-    def test_get_responses_query(self):
+#     def test_get_responses_query(self):
 
-        self.client = Client()
-        response = self.client.get('/api/sdcformresponse/', { 
-            'patientID': 'OH12344874', 'diagnosticProcedureID': 'adrenal', 'metadata': 'true' 
-            })
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue('sdcFormResponses' in json.loads(response.content))
+#         self.client = Client()
+#         response = self.client.get('/api/sdcformresponse/', { 
+#             'patientID': 'OH12344874', 'diagnosticProcedureID': 'adrenal', 'metadata': 'true' 
+#             })
+#         self.assertEqual(response.status_code, 200)
+#         self.assertTrue('sdcFormResponses' in json.loads(response.content))
 
     
-    def test_get_invalid_responseid(self):
-        self.client = Client()
-        response = self.client.get('/api/sdcformresponse/1234/')
-        self.assertEqual(response.status_code, 404)
+#     def test_get_invalid_responseid(self):
+#         self.client = Client()
+#         response = self.client.get('/api/sdcformresponse/1234/')
+#         self.assertEqual(response.status_code, 404)
 
-    def test_get_valid_response(self):
-        self.client = Client()
-        formID = upload_initial_form() 
-        data = { 
-            "patientID": "OH27891892",
-            "clinicianID": "CAMD92378223",
-            "sdcFormID": formID,
-        }
-        response = self.client.post("/api/sdcformresponse/", data)
-        sdcResponseId = json.loads(response.content)['responseObject']['id']
+#     def test_get_valid_response(self):
+#         self.client = Client()
+#         formID = upload_initial_form() 
+#         data = { 
+#             "patientID": "OH27891892",
+#             "clinicianID": "CAMD92378223",
+#             "sdcFormID": formID,
+#         }
+#         response = self.client.post("/api/sdcformresponse/", data)
+#         sdcResponseId = json.loads(response.content)['responseObject']['id']
         
-        response = self.client.get('/api/sdcformresponse/{}/'.format(sdcResponseId))
-        self.assertEqual(response.status_code, 200)
+#         response = self.client.get('/api/sdcformresponse/{}/'.format(sdcResponseId))
+#         self.assertEqual(response.status_code, 200)
 
-        self.assertTrue('responseObject' in json.loads(response.content))
+#         self.assertTrue('responseObject' in json.loads(response.content))
 
 
-# These tests include DELETE and PUT
-# PUT should have all body fields validated, if fields are missing or invalid, return 400 - Bad Request
-# DELETE and PUT return 404 if response id requested is non-existant
-class ModifyResponseTests(TestCase):
+# # These tests include DELETE and PUT
+# # PUT should have all body fields validated, if fields are missing or invalid, return 400 - Bad Request
+# # DELETE and PUT return 404 if response id requested is non-existant
+# class ModifyResponseTests(TestCase):
 
-    def test_delete_nonexistant_response(self):
-        self.client = Client()
-        response = self.client.delete('/api/sdcformresponse/1234/')
-        self.assertEqual(response.status_code, 404)
+#     def test_delete_nonexistant_response(self):
+#         self.client = Client()
+#         response = self.client.delete('/api/sdcformresponse/1234/')
+#         self.assertEqual(response.status_code, 404)
 
-    def test_delete_existing_response(self):
-        self.client = Client()
-        formID = upload_initial_form() 
-        data = { 
-            "patientID": "OH27891892",
-            "clinicianID": "CAMD92378223",
-            "sdcFormID": formID,
-        }
-        response = self.client.post("/api/sdcformresponse/", data)
-        sdcResponseId = json.loads(response.content)['responseObject']['id']
+#     def test_delete_existing_response(self):
+#         self.client = Client()
+#         formID = upload_initial_form() 
+#         data = { 
+#             "patientID": "OH27891892",
+#             "clinicianID": "CAMD92378223",
+#             "sdcFormID": formID,
+#         }
+#         response = self.client.post("/api/sdcformresponse/", data)
+#         sdcResponseId = json.loads(response.content)['responseObject']['id']
         
-        response = self.client.delete('/api/sdcformresponse/{}/'.format(sdcResponseId))
-        self.assertEqual(response.status_code, 200)
+#         response = self.client.delete('/api/sdcformresponse/{}/'.format(sdcResponseId))
+#         self.assertEqual(response.status_code, 200)
 
-    def test_update_nonexistant_response(self):
-        sdcResponse = generate_sdcresponse() 
-        put_response = self.client.put("/api/sdcformresponse/1000/", json.dumps(sdcResponse), content_type="application/json")
-        self.assertEqual(put_response.status_code, 404) # response 1000 does not exist
+#     def test_update_nonexistant_response(self):
+#         sdcResponse = generate_sdcresponse() 
+#         put_response = self.client.put("/api/sdcformresponse/1000/", json.dumps(sdcResponse), content_type="application/json")
+#         self.assertEqual(put_response.status_code, 404) # response 1000 does not exist
 
-     # Test checks if response id in the object body matches one passed in URL
-    def test_update_not_a_match(self):
-        sdcResponse1 = generate_sdcresponse()
-        sdcResponse2 = generate_sdcresponse()
+#      # Test checks if response id in the object body matches one passed in URL
+#     def test_update_not_a_match(self):
+#         sdcResponse1 = generate_sdcresponse()
+#         sdcResponse2 = generate_sdcresponse()
 
-        self.assertNotEqual(sdcResponse1["id"], sdcResponse2["id"])
-        # We are updating sdcResponse2 with sdcResponse1, but they have different response id's
-        put_response = self.client.put("/api/sdcformresponse/{}/".format(sdcResponse2['id']), 
-                                        json.dumps(sdcResponse1), content_type="application/json")
-        self.assertEqual(put_response.status_code, 400)
+#         self.assertNotEqual(sdcResponse1["id"], sdcResponse2["id"])
+#         # We are updating sdcResponse2 with sdcResponse1, but they have different response id's
+#         put_response = self.client.put("/api/sdcformresponse/{}/".format(sdcResponse2['id']), 
+#                                         json.dumps(sdcResponse1), content_type="application/json")
+#         self.assertEqual(put_response.status_code, 400)
 
-    def test_invalid_put_body(self):
-        sdcResponse = generate_sdcresponse()
-        body = { # Missing lots of fields
-            "id": sdcResponse['id'],
-            "patientID": sdcResponse['patientId'],
-        }
-        put_response = self.client.put("/api/sdcformresponse/{}/".format(sdcResponse['id']), json.dumps(body), 
-                                        content_type="application/json")
-        self.assertEqual(put_response.status_code, 400)
+#     def test_invalid_put_body(self):
+#         sdcResponse = generate_sdcresponse()
+#         body = { # Missing lots of fields
+#             "id": sdcResponse['id'],
+#             "patientID": sdcResponse['patientId'],
+#         }
+#         put_response = self.client.put("/api/sdcformresponse/{}/".format(sdcResponse['id']), json.dumps(body), 
+#                                         content_type="application/json")
+#         self.assertEqual(put_response.status_code, 400)
 
-    def test_update_valid(self):
-        sdcResponse = generate_sdcresponse()
-        put_response = self.client.put("/api/sdcformresponse/{}/".format(sdcResponse['id']), json.dumps(sdcResponse), 
-            content_type="application/json")
-        self.assertEqual(put_response.status_code, 200)
-        jsonData = json.loads(put_response.content)
-        self.assertTrue('responseObject' in jsonData)
-        self.assertTrue('invalidInputs' in jsonData)
-        self.assertTrue(len(jsonData['invalidInputs']) > 0) 
-        # The initial default response should have multiple invalid inputs
+#     def test_update_valid(self):
+#         sdcResponse = generate_sdcresponse()
+#         put_response = self.client.put("/api/sdcformresponse/{}/".format(sdcResponse['id']), json.dumps(sdcResponse), 
+#             content_type="application/json")
+#         self.assertEqual(put_response.status_code, 200)
+#         jsonData = json.loads(put_response.content)
+#         self.assertTrue('responseObject' in jsonData)
+#         self.assertTrue('invalidInputs' in jsonData)
+#         self.assertTrue(len(jsonData['invalidInputs']) > 0) 
+#         # The initial default response should have multiple invalid inputs
