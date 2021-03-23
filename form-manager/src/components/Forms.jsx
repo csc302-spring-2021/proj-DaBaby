f9bd3adbddf8b67180254d5d0458195abbd625d2
Adrian Zhao <acceleratorsnow@gmail.com>
2021-03-23 16:27:07 -0400

    Better error message. async function fix.

--- form-manager/src/components/Forms.jsx
+++ form-manager/src/components/Forms.jsx
@@ -4,35 +4,42 @@ import "./Forms.scss";
 import {getSDCForm} from "../actions/Actions";
 
 class Forms extends React.Component {
-	constructor(props) {
-		super(props);
+    constructor(props) {
+        super(props);
 
-		this.state = {
-			form: null,
-			id: this.props.location.state.id,
-		}
+        this.state = {
+            form: null,
+            id: this.props.location.state.id,
+        }
+    }
 
-		console.log(this.state.id)
-	}
+    componentDidMount() {
+        if (this.props.location) {
+            getSDCForm(this, this.state.id)
+        }
+    }
 
-	componentDidMount() {
-		if (this.props.location) {
-			getSDCForm(this, this.state.id)
-		}
-	}
+    render() {
+        console.log(this.state.form);
 
-	render() {
-		return (
-			(this.state.form ? (<div className={"Form-Container"}>
-				<h2 id="form-name-display">{this.state.form.name}</h2>
-				{this.state.form.sections.map((section) => (
-					<SDCSection section={section} key={section.id}/>
-				))}
-			</div>) : (<div>
-				<h1>SDCForm is invalid</h1>
-			</div>))
-		);
-	}
+        if (this.state.form) {
+        	if (!this.state.form.section || this.state.form.section.length === 0){
+        		return <div className={"Form-Container"}>
+					<h2 id="form-name-display">{this.state.form.name}</h2>
+					<h4 id="form-empty-error">The form is empty or may be corrupted!</h4>
+				</div>;
+			}
+
+            return <div className={"Form-Container"}>
+                <h2 id="form-name-display">{this.state.form.name}</h2>
+                {this.state.form.sections.map((section) => (
+                    <SDCSection section={section} key={section.id}/>
+                ))}
+            </div>;
+        }
+
+        return null;
+    }
 }
 
 export default Forms;
