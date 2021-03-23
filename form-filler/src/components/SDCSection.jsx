2d0e898b190f83a35a75f71a411287c1653f57a8
ruemu-digba <ruemu.digba@mail.utoronto.ca>
2021-03-22 21:27:14 -0400

    finished formatting response object to send to backend
    will get patientID and clinicianID later on when starting a new form gets completed

--- form-filler/src/components/SDCSection.jsx
+++ form-filler/src/components/SDCSection.jsx
@@ -17,20 +17,14 @@ import { Form, Field } from "react-final-form";
 import Question from "./Question";
 
 class SDCSection extends React.Component {
-
   // Prepare the values into a JSON that'll be sent to the backend
   onSubmit = async (values) => {
-    console.log("values")
-    console.log(values);
-    console.log("questionAnswerList");
     // This list will hold a list of questionAnswerObjects
     const questionAnswerList = [];
 
     // Loop through the values object that is essentially the questionAnswer to each question and each question id
     // Then prepare them and add them to the questionAnswerObject
     for (const property in values) {
-      // console.log(property.slice(6));
-      // console.log(values[property]);
       // Creating response object that'll be sent to the backend
       const questionAnswerObject = {};
 
@@ -43,86 +37,97 @@ class SDCSection extends React.Component {
         const { section, name, section_name } = this.props;
         const { questions } = section;
 
-        // Loop through questions from section to match up with the value from the form, and see if that question is 
+        // Loop through questions from section to match up with the value from the form, and see if that question is
         // single-choice or multiple-choice, if so then they need a section field in their questionAnswerObject
         for (let i = 0; i < questions.length; i++) {
-            if (questions[i]["id"] == questionID && questions[i]["type"] == "single-choice") {
-                questionAnswerObject["questionID"] = questionID;
-                questionAnswerObject["answer"] = {"selection": values[property]};
-                single_or_multiple_choice_question = "single-choice";
-            }
-            if (questions[i]["id"] == questionID && questions[i]["type"] == "multiple-choice") {
-              questionAnswerObject["questionID"] = questionID;
-              single_or_multiple_choice_question = "multiple-choice";
+          if (
+            questions[i]["id"] == questionID &&
+            questions[i]["type"] == "single-choice"
+          ) {
+            questionAnswerObject["questionID"] = questionID;
+            questionAnswerObject["answer"] = { selection: values[property] };
+            single_or_multiple_choice_question = "single-choice";
+          }
+          if (
+            questions[i]["id"] == questionID &&
+            questions[i]["type"] == "multiple-choice"
+          ) {
+            questionAnswerObject["questionID"] = questionID;
+            single_or_multiple_choice_question = "multiple-choice";
           }
         }
 
         if (!single_or_multiple_choice_question) {
           questionAnswerObject["questionID"] = property.slice(6);
-        questionAnswerObject["answer"] = values[property];
-        questionAnswerList.push(questionAnswerObject); // Add free-text/integer/true-false questionID and answer to the list
-        }
-
-        else if (single_or_multiple_choice_question == "single-choice") {
+          questionAnswerObject["answer"] = values[property];
+          questionAnswerList.push(questionAnswerObject); // Add free-text/integer/true-false questionID and answer to the list
+        } else if (single_or_multiple_choice_question == "single-choice") {
           questionAnswerObject["questionID"] = property.slice(6);
           questionAnswerObject["answer"]["selection"] = values[property];
           questionAnswerList.push(questionAnswerObject); // Add single-choice questionID and answer to the list
-        }
-
-        else if (single_or_multiple_choice_question == "multiple-choice") {
-          
-            // Loop through array of values gotten form values[property]
-            let first = true
-            for (let i = 0; i < values[property].length; i++) {
-                // If first value added to questionAnswerObject, create a new array for the answer field
-                if (first) {
-                  questionAnswerObject["questionID"] = property.slice(6);
-                  questionAnswerObject["answer"] = [{"selection": values[property][i]}];
-                  first = false;
-                }
-                // Otherwise push it to list of answers
-                else {
-                  questionAnswerObject["answer"].push({"selection": values[property][i]});
-                }
+        } else if (single_or_multiple_choice_question == "multiple-choice") {
+          // Loop through array of values gotten form values[property]
+          let first = true;
+          for (let i = 0; i < values[property].length; i++) {
+            // If first value added to questionAnswerObject, create a new array for the answer field
+            if (first) {
+              questionAnswerObject["questionID"] = property.slice(6);
+              questionAnswerObject["answer"] = [
+                { selection: values[property][i] },
+              ];
+              questionAnswerList.push(questionAnswerObject); // Add multiple-choice questionID and answer to the list
+              first = false;
             }
-            questionAnswerList.push(questionAnswerObject); // Add multiple-choice questionID and answer to the list
+            // Otherwise push it to list of answers
+            else {
+              questionAnswerObject["answer"].push({
+                selection: values[property][i],
+              });
+            }
+          }
         }
-
-
       }
 
       // Otherwise it is an addition
       else {
-
         // If the addition is a single choice question handle it this way
         const questionID = property.slice(22, 27); // Parse the question id from the property
         const question = property.slice(27); // Parse the question from the property
 
         // Find existingQuestionAnswerObject from the questionAnswerList so we can include the addition field
-        const existingQuestionAnswerObject = questionAnswerList.find(obj => {
-          return obj.questionID === questionID 
-        })
+        const existingQuestionAnswerObject = questionAnswerList.find((obj) => {
+          return obj.questionID === questionID;
+        });
 
         // If the question we are adding the addition to is an array (meaning multiple-choice question), do this
         if (Array.isArray(existingQuestionAnswerObject["answer"])) {
-
-            // Find existingAnswerObject from existing answer list so we can include the addition field
-            // to the specific answer in that list
-            const existingAnswerObject = existingQuestionAnswerObject["answer"].find(obj => {
-              return obj.selection === question
-            })
-            existingAnswerObject["addition"] = values[property] // Add the addition field to that answer
+          // Find existingAnswerObject from existing answer list so we can include the addition field
+          // to the specific answer in that list
+          const existingAnswerObject = existingQuestionAnswerObject[
+            "answer"
+          ].find((obj) => {
+            return obj.selection === question;
+          });
+          existingAnswerObject["addition"] = values[property]; // Add the addition field to that answer
         }
         // Otherwise do this (single-choice question)
         else {
           existingQuestionAnswerObject["answer"]["addition"] = values[property]; // Add the addition field to that answer
         }
-        
       }
-      
-    
-      console.log(questionAnswerList) // Print response 
     }
+    // Start building the answer response object to send to the backend
+    const answerResponseObject = {};
+
+    // Get useful form properties from prop
+    const { id } = this.props.sdcForm;
+    answerResponseObject["id"] = 2468;
+    answerResponseObject["answers"] = questionAnswerList;
+    answerResponseObject["patientID"] = "OH27891892";
+    answerResponseObject["clinicianID"] = "YP27923782";
+    answerResponseObject["sdcFormID"] = id;
+
+    console.log(answerResponseObject) // Print formatted response
   };
 
   /**
